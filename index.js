cf4a341cd445eb47b9c7facbefa1d33d4f05be74
Harmansate <130687166+harmansate@users.noreply.github.com>
2024-03-08 19:25:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const opn = require('opn')
 
-const url = 'https://github.com/harmansate'
+const url = 'https://github.com/harmansate/simple-sky'
 
 opn(url, { wait: false })
     .then(() => console.log(`opening ${url}`))
